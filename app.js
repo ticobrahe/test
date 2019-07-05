a80e95abf4bb5f1e82e87de2324ee5dae49c38e3
ticobrahe <olabiyi.sam@gmail.com>
2019-07-05 20:56:07 +0100

    Initial Setup

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import  dotenv from 'dotenv'
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+dotenv.config();
+
+app.use('/', (req,res) => {
+    res.send({
+        name: 'Samel',
+        id: 2
+    });
+})
+
+app.listen(port, () => {
+    console.log(port);
+} )
