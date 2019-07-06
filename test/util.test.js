6598f0135ec6dbd378587cc91128d62385a3b8d0
ticobrahe <olabiyi.sam@gmail.com>
2019-07-06 20:24:51 +0100

    modified test file

--- test/util.test.js
+++ test/util.test.js
@@ -8,12 +8,12 @@ it('should add two number', () => {
   }
 });
 
-// const assert = require('assert');
+const assert = require('assert');
 
-// describe('Array', () => {
-//   describe('#indexOf()', () => {
-//     it('should return -1 when the value is not present', ()=> {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
-//     });
-//   });
-// });
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', ()=> {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
